bac7e2712f52580faacada0be711f44defa40f46
yousufislamme <your_email@youremail.com>
2022-03-17 02:15:46 +0600

    menuBar v1.5

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-let menuBar = document.querySelector('.toggle');
-   menuBar.onclick = function(){
-   menuBar.classList.toggle('active');
-   console.log('Work');
-}
+// MenuBar
+let menuBar = document.querySelector('.toggle')
+menuBar.onclick = function () {
+  menuBar.classList.toggle('active');
+};
+// let showMenuBar = document.querySelector('.menu_items_box')
+// showMenuBar.onclick = function () {
+//   showMenuBar.classList.menu_items_box('active');
+// };
+
+// Show Menu
+(function () {
+
+  var button = document.querySelector('.toggle');
+  var box = document.querySelector('.menu_items_box');
+  
+  button.addEventListener('click', function () {
+
+    box.classList.toggle('active');
+
+  });
+  
+})();
